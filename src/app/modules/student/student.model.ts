ff1f8231eec2821a8fd8e819bfc8454f20ea190e
unknown <infohasnat@gmail.com>
2025-07-28 16:50:13 +0600

    5th

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -1,7 +1,77 @@
 import { Schema } from 'mongoose'
-import { Student } from './student.interface'
+import { Student, UserName } from './student.interface'
+
+const UserNameSchema = new Schema<UserName>({
+  firstName: { type },
+})
 
 const StudentSchema = new Schema<Student>({
   id: { type: String },
-  name: {},
+  name: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    middleName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  gender: ['male', 'female'],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  //emergencyContactNo: { type: String, required: true },
+  presentAddress: { type: String, required: true },
+  parmanentAddress: { type: String, required: true },
+  guardian: {
+    fatherName: {
+      type: String,
+      required: true,
+    },
+    fatherOccupation: {
+      type: String,
+      required: true,
+    },
+    fatherContactNo: {
+      type: String,
+      required: true,
+    },
+    motherName: {
+      type: String,
+      required: true,
+    },
+    motherOccupation: {
+      type: String,
+      required: true,
+    },
+    motherContactNo: {
+      type: String,
+      required: true,
+    },
+  },
+  localGuardian: {
+    name: {
+      type: String,
+      required: true,
+    },
+    occupation: {
+      type: String,
+      required: true,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  profileImg: { type: String },
+  isActive: ['active', 'blocked'],
 })
