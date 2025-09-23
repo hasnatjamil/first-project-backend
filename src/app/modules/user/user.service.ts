ca641012e7999e3d1f5c735aa8142fa882dfbd1d
hasnatjamil <139052903+hasnatjamil@users.noreply.github.com>
2025-09-23 23:57:24 +0600

    27th commit impl transaction & Rollback

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,12 +1,13 @@
+import mongoose from 'mongoose'
 import config from '../../config'
 import AppError from '../../errors/AppError'
-import { TAcademicSemester } from '../academicSemester/academicSemester.interface'
 import { AcademicSemester } from '../academicSemester/academicSemester.model'
 import { TStudent } from '../student/student.interface'
 import { Student } from '../student/student.model'
 import { TUser } from './user.interface'
 import { User } from './user.model'
 import { generateStudentId } from './user.utils'
+import httpStatus from 'http-status'
 
 const createStudentIntoDB = async (password: string, payLoad: TStudent) => {
   // const result = await Student.create(student) create is a built in static method
@@ -25,21 +26,44 @@ const createStudentIntoDB = async (password: string, payLoad: TStudent) => {
   userData.role = 'student'
 
   //find academic semester info
-  const admissionSemester = await AcademicSemester.findById(payLoad.admissionSemester)
+  const admissionSemester = await AcademicSemester.findById(
+    payLoad.admissionSemester,
+  )
 
   if (!admissionSemester) {
-    throw new AppError(400,'Something went wrong!')
+    throw new AppError(400, 'Something went wrong!')
   }
-  userData.id = await generateStudentId(admissionSemester)
 
-  const newUser = await User.create(userData) //built in static method
+  const session = await mongoose.startSession()
+  try {
+    session.startTransaction()
+    //set generated id
+    userData.id = await generateStudentId(admissionSemester)
 
-  if (Object.keys(newUser).length) {
-    payLoad.id = newUser.id
-    payLoad.user = newUser._id
+    const newUser = await User.create([userData], { session }) //built in static method
 
-    const newStudent = await Student.create(payLoad)
-    return newStudent
+    // creating new user transaction-1
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user')
+    }
+
+    payLoad.id = newUser[0].id
+    payLoad.user = newUser[0]._id
+
+    // creating student transaction-2
+    const newStudent = await Student.create([payLoad], { session })
+
+    if (!newStudent.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user')
+    }
+    await session.commitTransaction()
+    return newStudent[0]
+  } catch (error) {
+    await session.abortTransaction()
+
+    throw error
+  } finally {
+    await session.endSession()
   }
 }
 
