32a2cf6a09c61d4daf3848e31208991276268afb
hasnatjamil <139052903+hasnatjamil@users.noreply.github.com>
2025-09-28 00:02:20 +0600

    29th commit impl transaction & Rollback refactoring

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+//New LOGIC
 import mongoose from 'mongoose'
 import config from '../../config'
 import AppError from '../../errors/AppError'
@@ -6,63 +7,71 @@ import { TStudent } from '../student/student.interface'
 import { Student } from '../student/student.model'
 import { TUser } from './user.interface'
 import { User } from './user.model'
-import { generateStudentId } from './user.utils'
+
 import httpStatus from 'http-status'
+import { generateStudentId } from './user.utils'
 
-const createStudentIntoDB = async (password: string, payLoad: TStudent) => {
-  // const result = await Student.create(student) create is a built in static method
-
-  //   if (await Student.isUserExist(studentData.id)) {
-  //     throw new Error('User Already Exist!')
-  //   }
-
-  //create user object
+// Service function: নতুন student create করার জন্য
+const createStudentIntoDB = async (
+  password: string | undefined,
+  payLoad: TStudent,
+) => {
+  // userData অবজেক্ট তৈরি করা হচ্ছে যাতে নতুন User এর ডাটা থাকবে
   const userData: Partial<TUser> = {}
 
-  //if password not given, use default password
-
-  userData.password = password || (config.default_password as string)
-
+  // Student role assign করা হচ্ছে
   userData.role = 'student'
 
-  //find academic semester info
+  // 👉 যদি password request থেকে না আসে, তাহলে default password use করবে
+  userData.password = password || (config.default_password as string)
+
+  // Admission semester validate করা হচ্ছে
   const admissionSemester = await AcademicSemester.findById(
     payLoad.admissionSemester,
   )
 
   if (!admissionSemester) {
-    throw new AppError(400, 'Something went wrong!')
+    throw new AppError(httpStatus.BAD_REQUEST, 'Something went wrong!')
   }
 
+  // Transaction শুরু
   const session = await mongoose.startSession()
   try {
     session.startTransaction()
-    //set generated id
-    userData.id = await generateStudentId(admissionSemester)
 
-    const newUser = await User.create([userData], { session }) //built in static method
+    // Generate unique student ID (based on semester)
+    userData.id = await generateStudentId(admissionSemester, session)
 
-    // creating new user transaction-1
+    //  Step-1: User collection এ নতুন User তৈরি
+    const newUser = await User.create([userData], { session })
     if (!newUser.length) {
       throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user')
     }
 
+    //  Step-2: Student payload এ user id এবং reference যোগ করা
     payLoad.id = newUser[0].id
     payLoad.user = newUser[0]._id
 
-    // creating student transaction-2
+    //  Step-3: Student collection এ নতুন student তৈরি
     const newStudent = await Student.create([payLoad], { session })
-
     if (!newStudent.length) {
-      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user')
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student')
     }
+
+    // Transaction commit (সব ঠিক থাকলে DB তে final save হবে)
     await session.commitTransaction()
+
+    // প্রথম student অবজেক্ট return করা
     return newStudent[0]
   } catch (error) {
+    
+    // কোন error হলে পুরো transaction rollback করবে
     await session.abortTransaction()
+    console.error('Error in createStudentIntoDB:', error) // debug log
 
-    throw error
+    //throw error
   } finally {
+    // session end করা হচ্ছে
     await session.endSession()
   }
 }
@@ -70,3 +79,77 @@ const createStudentIntoDB = async (password: string, payLoad: TStudent) => {
 export const UserServices = {
   createStudentIntoDB,
 }
+
+
+// OLD LOGIC
+// import mongoose from 'mongoose'
+// import config from '../../config'
+// import AppError from '../../errors/AppError'
+// import { AcademicSemester } from '../academicSemester/academicSemester.model'
+// import { TStudent } from '../student/student.interface'
+// import { Student } from '../student/student.model'
+// import { TUser } from './user.interface'
+// import { User } from './user.model'
+// import { generateStudentId } from './user.utils'
+// import httpStatus from 'http-status'
+
+// const createStudentIntoDB = async (password: string, payLoad: TStudent) => {
+//   // const result = await Student.create(student) create is a built in static method
+
+//   //   if (await Student.isUserExist(studentData.id)) {
+//   //     throw new Error('User Already Exist!')
+//   //   }
+
+//   //create user object
+//   const userData: Partial<TUser> = {}
+
+//   //if password not given, use default password
+
+//   userData.password = password || (config.default_password as string)
+
+//   userData.role = 'student'
+
+//   //find academic semester info
+//   const admissionSemester = await AcademicSemester.findById(
+//     payLoad.admissionSemester,
+//   )
+
+//   if (!admissionSemester) {
+//     throw new AppError(400, 'Something went wrong!')
+//   }
+
+//   const session = await mongoose.startSession()
+//   try {
+//     session.startTransaction()
+//     //set generated id
+//     userData.id = await generateStudentId(admissionSemester)
+
+//     const newUser = await User.create([userData], { session }) //built in static method
+//     // creating new user transaction-1
+//     if (!newUser.length) {
+//       throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user')
+//     }
+
+//     payLoad.id = newUser[0].id
+//     payLoad.user = newUser[0]._id
+
+//     // creating student transaction-2
+//     const newStudent = await Student.create([payLoad], { session })
+//     if (!newStudent.length) {
+//       throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user')
+//     }
+
+//     await session.commitTransaction()
+//     return newStudent[0]
+//   } catch (error) {
+//     await session.abortTransaction()
+
+//     throw error
+//   } finally {
+//     await session.endSession()
+//   }
+// }
+
+// export const UserServices = {
+//   createStudentIntoDB,
+// }
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,45 +1,79 @@
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface'
-import { User } from './user.model'
+//import { User } from './user.model'
+import { Schema, model } from 'mongoose'
 
-const findLastStudentId = async () => {
-  const lastStudentId = await User.findOne(
-    {
-      role: 'student',
-    },
-    { id: 1, _id: 0 },
-  )
-    .sort({
-      createdAt: -1,
-    })
-    .lean()
-  return lastStudentId?.id ? lastStudentId.id : undefined
+//Old Logic
+// const findLastStudentId = async () => {
+//   const lastStudentId = await User.findOne(
+//     {
+//       role: 'student',
+//     },
+//     { id: 1, _id: 0 },
+//   )
+//     .sort({
+//       createdAt: -1,
+//     })
+//     .lean()
+//   return lastStudentId?.id ? lastStudentId.id : undefined
+// }
+
+// //year semester 4 digits number
+// export const generateStudentId = async (payLoad: TAcademicSemester) => {
+//   // const currentId = (await findLastStudentId()) || (0).toString()
+//   // let incrementedId = (Number(currentId) + 1).toString().padStart(4, '0')
+
+//   //for fixing bug
+//   let currentId = (0).toString() // 0000 by default
+//   const lastStudentId = await findLastStudentId() //2030 01 0001
+
+//   const lastStudentSemesterCode = lastStudentId?.substring(4, 5) //01
+//   const lastStudentYear = lastStudentId?.substring(0, 4)  //2030
+
+//   const currentSemesterCode = payLoad.code //01
+//   const currentYear = payLoad.year  //2030
+
+//   if (
+//     lastStudentId &&
+//     lastStudentSemesterCode === currentSemesterCode &&
+//     lastStudentYear === currentYear
+//   ) {
+//     currentId = lastStudentId.substring(6)  //0001
+//   }
+
+//   let incrementedId = (Number(currentId) + 1).toString().padStart(4, '0')
+//   incrementedId = `${payLoad.year}${payLoad.code}${incrementedId}`
+//   return incrementedId
+// }
+
+//New Logic
+// Counter type
+type TCounter = {
+  id: string // যেমন "student-203001"
+  seq: number
 }
 
-//year semester 4 digits number
-export const generateStudentId = async (payLoad: TAcademicSemester) => {
-  // const currentId = (await findLastStudentId()) || (0).toString()
-  // let incrementedId = (Number(currentId) + 1).toString().padStart(4, '0')
+// Counter schema
+const counterSchema = new Schema<TCounter>({
+  id: { type: String, required: true, unique: true },
+  seq: { type: Number, default: 0 },
+})
 
-  //for fixing bug
-  let currentId = (0).toString() // 0000 by default
-  const lastStudentId = await findLastStudentId() //2030 01 0001
-  
+const Counter = model<TCounter>('Counter', counterSchema)
 
-  const lastStudentSemesterCode = lastStudentId?.substring(4, 5) //01
-  const lastStudentYear = lastStudentId?.substring(0, 4)  //2030
+// generate student ID with session support
+export const generateStudentId = async (
+  semester: TAcademicSemester,
+  session: any,
+) => {
+  const prefix = `${semester.year}${semester.code}` // যেমন 203001
 
-  const currentSemesterCode = payLoad.code //01
-  const currentYear = payLoad.year  //2030
+  // session-aware counter update
+  const counter = await Counter.findOneAndUpdate(
+    { id: `student-${prefix}` },
+    { $inc: { seq: 1 } },
+    { new: true, upsert: true, session }, // ✅ session add করলাম
+  )
 
-  if (
-    lastStudentId &&
-    lastStudentSemesterCode === currentSemesterCode &&
-    lastStudentYear === currentYear
-  ) {
-    currentId = lastStudentId.substring(6)  //0001
-  }
-
-  let incrementedId = (Number(currentId) + 1).toString().padStart(4, '0')
-  incrementedId = `${payLoad.year}${payLoad.code}${incrementedId}`
-  return incrementedId
+  const incrementedId = counter.seq.toString().padStart(4, '0') // 0001, 0002
+  return `${prefix}${incrementedId}`
 }
